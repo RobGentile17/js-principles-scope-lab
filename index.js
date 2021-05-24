0a3b7e16888e04dc371eb190653127461dfb537e
Rob <robert.gentile@powerhrg.com>
2021-05-24 12:06:29 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+ var customerName = 'bob';
+
+    function upperCaseCustomerName() {
+        return customerName = 'BOB'
+    }
+    function setBestCustomer() {
+      return bestCustomer = 'not bob'
+    }
+    function overwriteBestCustomer() {
+        return bestCustomer = 'maybe bob'
+    }
+    function changeLeastFavoriteCustomer() {
+        
+    }
+    const leastFavoriteCustomer = 'rob'
+    function changeLeastFavoriteCustomer() {
+      return leastFavoriteCustomer = 'jelena'
+    }
